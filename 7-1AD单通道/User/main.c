a32a82364eef9f9f195b6b6e045ab2d6523790e8
JKzhangt <1654047433@qq.com>
2023-02-16 16:04:00 +0800

    7-1ADC单通道
    通过电位器输出的电位器输出的模拟量转换为数据显示

---
+++ 7-1AD单通道/User/main.c
@@ -0,0 +1,31 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "LED.h"
+#include "Key.h"
+#include "OLED.h"
+#include "AD.h"
+
+uint16_t ADValue;
+float Voltage;
+
+int main(void)
+{
+	OLED_Init();
+	AD_Init();
+
+	OLED_ShowString(1,1,"ADValue:");
+	OLED_ShowString(2,1,"Voltage:0.00v");
+	while(1)
+	{
+		ADValue=AD_GetValue();
+		Voltage=(float)ADValue/4095 *3.3;
+		
+		OLED_ShowNum(1,9,ADValue,4);
+		OLED_ShowNum(2,9,Voltage,1);
+		OLED_ShowNum(2,11,(uint16_t)(Voltage*100)%100,2);
+		
+		Delay_ms(100);
+		
+	}
+	
+}
